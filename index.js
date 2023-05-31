af2b0f8e5252ac4a53d0e7a32fc371c880b0c8e4
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-31 13:59:09 +0600

    jwt-secret-token create

--- index.js
+++ index.js
@@ -34,12 +34,30 @@ async function run() {
 
 
         // user relate data
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result);
+        })
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: "admin"
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: "User is already exist"});
+            if (existingUser) {
+                return res.send({ message: "User is already exist" });
             }
             const result = await usersCollection.insertOne(user);
             res.send(result);
